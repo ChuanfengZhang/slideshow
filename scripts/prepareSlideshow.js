7ef60b2e2ff5a5e328a568521afb4cad71a906e2
Chuanfeng Zhang <wfhtzcf@gmail.com>
2016-10-07 15:50:53 +0800

    Update prepareSlideshow.js

--- scripts/prepareSlideshow.js
+++ scripts/prepareSlideshow.js
@@ -1,27 +1,30 @@
-function prepareSlideshow(){
-//Make sure the browser understands the DOM methods
-  if(!document.createElement || !document.getElementById || !document.getElementsByTagName) {
+function prepareSlideshow() {
+  //Make sure the browser understands the DOM methods
+  if (!document.createElement || !document.getElementById || !document.getElementsByTagName) {
     return false;
   }
-//Make sure the Elements exist
-  if(!document.getElementById("linklist")) return false;
+  //Make sure the Elements exist
+  if (!document.getElementById("linklist")) return false;
   var slideshow = document.createElement("div");
-  slideshow.setAttribute("id","slideshow");
+  slideshow.setAttribute("id", "slideshow");
   var preview = document.createElement("img");
-  preview.setAttribute("id","preview");
-  preview.setAttribute("src","topics.gif");
-  preview.setAttribute("alt","building blocks of web design");
+  preview.setAttribute("id", "preview");
+  preview.setAttribute("src", "topics.gif");
+  preview.setAttribute("alt", "building blocks of web design");
   slideshow.appendChild(preview);
   var list = document.getElementById("linklist");
-  insertAfter(slideshow,list);
-//Get all lists
+  insertAfter(slideshow, list);
+  //Get all lists
   var links = document.getElementsByTagName("li");
-//Attach the animation behavior to the mouseover event
-  for(var i = 0; i < links.length; i ++){
-    links[i].onmouseover = function (){
-      var x = -(i + 1) * 100;
-      moveElement("preview",x,0,10);
-    }
+  //Attach the animation behavior to the mouseover event
+  links[0].onmouseover = function() {
+    moveElement("preview", -100, 0, 10);
+  }
+  links[1].onmouseover = function() {
+    moveElement("preview", -200, 0, 10);
+  }
+  links[2].onmouseover = function() {
+    moveElement("preview", -300, 0, 10);
   }
 }
 addLoadEvent(prepareSlideshow);
